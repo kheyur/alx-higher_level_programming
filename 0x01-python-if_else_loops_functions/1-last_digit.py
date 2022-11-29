6e9a0196acbb79f24ae236ff5962c9e1efa6e196
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-11-29 06:34:54 -0800

    last_num created to hold last digit of number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number[-1] == 0:
-    print(f'Last digit of {number} is {number[-1]} and is 0')
-elif number[-1] > 5:
-    print(f'Last digit of {number} is {number[-1]} and is greater than 5')
-elif number[-1] > 0 and number[-1] < 6:
-    print(f'Last digit of {number} is {number[-1]} and is less than 6 and not 0')
+last_num = number[-1]
+if last_num == 0:
+    print(f'Last digit of {number} is {last_num} and is 0')
+elif last_num > 5:
+    print(f'Last digit of {number} is {last_num} and is greater than 5')
+elif last_num > 0 and last_num < 6:
+    print(f'Last digit of {number} is {last_num} and is less than 6 and not 0')
