65d2dcec9b643bea97456500731ed4ba43b59b27
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-11-29 06:30:50 -0800

    correction to syntax

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,5 +5,5 @@ if number[-1] == 0:
     print(f'Last digit of {number} is {number[-1]} and is 0')
 elif number[-1] > 5:
     print(f'Last digit of {number} is {number[-1]} and is greater than 5')
-elif number[-1] > 0 && number[-1] < 6:
+elif number[-1] > 0 and number[-1] < 6:
     print(f'Last digit of {number} is {number[-1]} and is less than 6 and not 0')
