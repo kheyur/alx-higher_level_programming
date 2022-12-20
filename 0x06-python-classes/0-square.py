44deb596a4b4b1077cd29f9f69a8244cc077eae5
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-12-20 05:42:50 -0800

    correction

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Square module. This module contains a class that defines a square"""
+"""Square module. This module contains a class that defines a square
+"""
 
 class Square():
     """Class Square that defines a square.
