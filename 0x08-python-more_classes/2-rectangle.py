d869ad226c8c8780217db792fffc8db51c2911b0
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2023-01-03 15:46:59 -0800

    2-rectangle.py commit

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""Module containing Rectangle class"""
+
+
+class Rectangle:
+    """Class to create a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initialize a new rectangle with 'width' and 'height'.
+
+        Args:
+            width (int): width of rectangle with value >= 0.
+            height (int): height of rectangle with value >= 0.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """Getter returning value for 'height'.
+
+        Returns: value of 'height'.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter to define value for 'height'.
+
+        Args:
+            value (int): value to use for 'height'/
+
+        Raises:
+            TypeError: If 'value' is not of type int.
+            ValueError: If 'value' is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    @property
+    def width(self):
+        """Getter that returns value of 'width'.
+
+        Returns: value of 'width'.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter to define value for 'width'.
+
+        Args:
+            value (int): value to use for 'width'.
+
+        Raises:
+            TypeError: If 'value' is not of type int.
+            ValueError: If 'value' is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    def area(self):
+        """Method to calculate area of Rectangle instance"""
+        return self.width *self.height
+
+    def perimeter(self):
+        """Method to calculate perimeter of Rectangle instance
+
+        Retunr: 2 * (width + height) if both 'width' and 'height' > 0, else 0.
+        """
+        return 2*(self.width + self.height) * bool(self.width and self.height)
